6f43349c0ba9109a701d69964e04a21f999a5dce
cxdetest <106492725+cxdetest@users.noreply.github.com>
2023-05-25 04:42:46 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBzG2m0XsUjHVRqqQ3dx1vHEY2KZhgKjpE',
-  authDomain: 'fir-crud-react-4c434.firebaseapp.com',
-  projectId: 'fir-crud-react-4c434',
-  storageBucket: 'fir-crud-react-4c434.appspot.com',
-  messagingSenderId: '123631580363',
-  appId: '1:123631580363:web:a75b19fd1239b4b679574e',
-  measurementId: 'G-15HSY0RPMF',
+'replace with firebase credentials'
 };
 
 const app = initializeApp(firebaseConfig);
